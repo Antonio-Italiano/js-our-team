3ef5005a933eda3dd7a35ee4c35ca9af11727a00
Antonio-Italiano <antoitalia221@gmail.com>
2022-12-21 16:04:21 +0100

    Add array (objects)

--- js/script.js
+++ js/script.js
@@ -1,1 +1,18 @@
-console.log('ok')
+// CREO L'ARRAY DI OGGETTI CON LE INFORMAZIONI FORNITE
+const team = [
+        // CREO GLI OGGETTI
+        {fullName: 'Wayne Barnett',   role: 'Founder & CEO',        image: 'wayne-barnett-founder-ceo.jpg'},
+        {fullName: 'Angela Caroll',   role: 'Chief Editor',         image: 'wayne-barnett-founder-ceo.jpg'},
+        {fullName: 'Walter Gordon',   role: 'Office Manager',       image: 'wayne-barnett-founder-ceo.jpg'},
+        {fullName: 'Angela Lopez',    role: 'Social Media Manager', image: 'wayne-barnett-founder-ceo.jpg'},
+        {fullName: 'Scott Estrada',   role: 'Developer',            image: 'wayne-barnett-founder-ceo.jpg'},
+        {fullName: 'Barbara Ramos',   role: 'Graphic Designer',     image: 'wayne-barnett-founder-ceo.jpg'},
+    ]
+
+// CREO UN CICLO (FOR) PER MOSTRARE LE INFORMAZIONI IN CONSOLE 
+for (let i = 0; i < team.length; i++) {
+    console.table(
+        `full Name:` + ` ` + team[i].fullName, 
+        `Role:` + ` ` + team[i].role, 
+        `Image:` + ` ` + team[i].image,);
+}
